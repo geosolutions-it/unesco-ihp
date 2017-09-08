3fafd8151e268f81382fd9e5337d7bcc99f8fd75
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-09-08 16:04:16 +0200

    - French translations

--- ihp/content/forms.py
+++ ihp/content/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 import account.forms
 
 
 class SignupForm(account.forms.SignupForm):
+    first_name = forms.CharField(label=_(u'First name'))
+
+    last_name = forms.CharField(label=_(u'Last name'))
+
     accept = forms.BooleanField(
-        required=True,
-        label=mark_safe(
-            'I agree to the IHP-WINS <a href="/terms-of-use" target="_blank">Terms of Use</a>'
-        )
+        required = True,
+        label = _(u'I agree to the IHP-WINS Terms of use')
     )
