f3b213ca725339fdd206e2a5feaa6dd3f69eb346
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-12-18 12:26:33 +0100

    - fixes #93 2 users with the same username (#93)

--- ihp/content/adapters.py
+++ ihp/content/adapters.py
@@ -9,11 +9,14 @@ import string
 import logging
 import traceback
 
+from django import forms
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils.module_loading import import_string
+from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.account.utils import user_field
@@ -33,6 +36,14 @@ def populate_username(first_name, last_name):
     return filter(lambda x: x in printable, u'{}.{}'.format(fname, lname).encode('utf-8').strip())
 
 
+def get_user_lookup_kwargs(kwargs):
+    result = {}
+    username_field = getattr(get_user_model(), "USERNAME_FIELD", "username")
+    for key, value in kwargs.items():
+        result[key.format(username=username_field)] = value
+    return result
+
+
 class UnescoLocalAccountAdapter(LocalAccountAdapter):
     """Customizations for local accounts
 
@@ -47,17 +58,45 @@ class UnescoLocalAccountAdapter(LocalAccountAdapter):
         # validate the already generated username with django validation
         # if it passes use that, otherwise use django-allauth's way of
         # generating a unique username
+
+        # safe_username = user_username(user)
         try:
-            # safe_username = user_username(user)
             safe_username = populate_username(user_field(user, 'first_name'), user_field(user, 'last_name'))
+        except Exception, e:
+            traceback.print_exc()
+            raise forms.ValidationError(e)
+            # safe_username = self.generate_unique_username([
+            #     user_field(user, 'first_name'),
+            #     user_field(user, 'last_name'),
+            #     user_email(user),
+            #     user_username(user)
+            # ])
+
+        User = get_user_model()
+        lookup_kwargs = get_user_lookup_kwargs({
+            "{username}__iexact": safe_username
+        })
+        qs = User.objects.filter(**lookup_kwargs)
+
+        _user_name_is_valid = False
+        if not qs.exists():
             user.username = safe_username
             user.full_clean()
-        except ValidationError:
-            traceback.print_exc()
-            safe_username = self.generate_unique_username([
-                user_field(user, 'first_name'),
-                user_field(user, 'last_name'),
-                user_email(user),
-                user_username(user)
-            ])
-        user_username(user, safe_username)
+            user_username(user, safe_username)
+            _user_name_is_valid = True
+        else:
+            lookup_kwargs = get_user_lookup_kwargs({
+                "{username}__startswith": safe_username
+            })
+            qs = User.objects.filter(**lookup_kwargs)
+            safe_username += '_{}'.format((qs.count()))
+            try:
+                User.objects.get(username__iexact=safe_username)
+            except User.DoesNotExist:
+                user.username = safe_username
+                user.full_clean()
+                user_username(user, safe_username)
+                _user_name_is_valid = True
+
+        if not _user_name_is_valid:
+            raise forms.ValidationError(_("This username already exists."))
