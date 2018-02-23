8ba9a377ba013fb22a26505761cf734deb76917b
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-02-23 13:21:55 +0100

    Secure Django a bit more

--- ihp/settings.py
+++ ihp/settings.py
@@ -601,3 +601,15 @@ LOGGING = {
             "handlers": ["console"], "level": "DEBUG", },
         },
     }
+
+# Security stuff
+MIDDLEWARE_CLASSES += ('django.middleware.security.SecurityMiddleware',)
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = False
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = False
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
