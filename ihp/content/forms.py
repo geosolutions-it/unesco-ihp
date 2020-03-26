7f9bc636f2b36855c38e7c421e55d3849dc0bc61
abulojoshua1 <abulojoshua1@gmail.com>
2020-03-26 10:49:43 +0300

    Add more fields to the signup form

--- ihp/content/forms.py
+++ ihp/content/forms.py
@@ -7,6 +7,7 @@ from allauth.account import forms as account_forms
 from allauth.socialaccount import forms as socialaccount_forms
 
 from allauth.account.adapter import get_adapter
+from geonode.groups.models import GroupProfile
 
 
 class UnescoLocalAccountSignupForm(account_forms.SignupForm):
@@ -41,6 +42,19 @@ def _replace_username_with_first_last(form):
         required=False,
         label=_(u"Who recommended you IHP-WINS?"),
         widget=forms.TextInput(attrs={'placeholder': _("Name of the person")}))
+
+    fields["organization"] = forms.CharField(min_length=2, label=_(u"Organization"),
+        widget=forms.TextInput(attrs={'placeholder': _(u"Organization")}), required=False)
+    fields["position"] = forms.CharField(min_length=2, label=_(u"Position"),
+        widget=forms.TextInput(attrs={'placeholder': _(u"Position")}), required=False)
+    fields["country"] = forms.CharField(min_length=2, label=_(u"Country"),
+        widget=forms.TextInput(attrs={'placeholder': _(u"Country")}), required=False)
+
+    fields["request_to_join_group"] = forms.ModelMultipleChoiceField(
+        queryset=GroupProfile.objects.all(),
+        required=False,
+        label=_(u"Group(s) you want to join"))
+
     terms_url = "%s/terms-of-use" % settings.SITEURL
     terms_href = "<a href={!r} target='_blank' rel='noopener noreferrer'>".format(terms_url)
     terms_href = "%s%s%s" % (terms_href, _("IHP-WINS Terms of use"), "<a>")
