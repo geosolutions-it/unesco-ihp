f7cca0d45c3fe9728f5e15c2a6316ab8d1406e92
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-01 12:03:50 +0300

    Create survey model and surver managing admin singleton model

---
+++ ihp/survey/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from geonode.singleton import SingletonModel
+from datetime import timedelta
+
+
+class Survey(models.Model):
+    name = models.CharField(max_length=255)
+    organization = models.CharField(max_length=255)
+    email = models.EmailField()
+    country = models.CharField(max_length=255)
+    reason_for_data_download = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return u'Survey pk=[{}]'.format(self.pk)
+
+
+class SurveyConfiguration(SingletonModel):
+    cookie_expiration_time = models.DurationField(default=timedelta(hours=24))
+    survey_enabled = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'SurveyConfiguration'
+
+    def __str__(self):
+        return 'SurveyConfiguration'
