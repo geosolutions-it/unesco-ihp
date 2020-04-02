965ac451793946fc9ae0170fd38b7d568f89fc56
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-02 21:43:46 +0300

    Add docstrings to tests

--- ihp/survey/tests/test_survey_view.py
+++ ihp/survey/tests/test_survey_view.py
@@ -7,12 +7,12 @@ import urllib.parse
 from http.cookies import SimpleCookie
 
 import pytest
+from django import forms
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from ihp.survey.models import Survey, SurveyConfiguration
-from django.contrib.auth import get_user_model
-from django import forms
 
 
 @pytest.mark.django_db
@@ -37,6 +37,9 @@ class TestSurveyView(TestCase):
         }
 
     def test_successful_download_with_existing_cookies(self):
+        """
+        Test that a user with survey cookies and has filled in form correctly can download a resource
+        """
         self.client.cookies = SimpleCookie({"ihp_dlsurvey": "ihp_dlsurvey"})
         response = self.client.get(
             u"{}?download_url={}&next={}".format(
@@ -45,44 +48,65 @@ class TestSurveyView(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_form_rendering_with_missing_survey_cookies(self):
+        """
+        Test that a user with with no survey cookies view the download survey form
+        """
         response = self.client.get(
             u"{}?download_url={}&next={}".format(
                 self.survey_route, urllib.parse.quote("http://example.com"), "/")
         )
         self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.context["form"])
 
     def test_survey_form_prefilled_values_for_authenticate_users(self):
+        """
+        Test that an authenticated user receives a form with some fields pre-filled but hidden
+        """
         user = get_user_model()
         user = user.objects.create_user(username="Thiago.Silver", email="Thiago@Silver.com",
-                                 country="DZA", organization="PSG", password="very-secret")
+                                        country="DZA", organization="PSG", password="very-secret")
 
         self.client.login(username=user.username, password="very-secret")
         response = self.client.get(
             u"{}?download_url={}&next={}".format(
                 self.survey_route, urllib.parse.quote("http://example.com"), "/")
         )
-        self.assertEqual(response.context["form"].fields['name'].initial, user.username)
-        self.assertEqual(response.context["form"].fields['email'].initial, user.email)
-        self.assertEqual(response.context["form"].fields['country'].initial, user.country)
-        self.assertEqual(response.context["form"].fields['organization'].initial, user.organization)
-
+        self.assertEqual(
+            response.context["form"].fields['name'].initial, user.username)
+        self.assertEqual(
+            response.context["form"].fields['email'].initial, user.email)
+        self.assertEqual(
+            response.context["form"].fields['country'].initial, user.country)
+        self.assertEqual(
+            response.context["form"].fields['organization'].initial, user.organization)
 
     def test_survey_hidden_form_fields_for_authenticate_users(self):
+        """
+        Test that an authenticated user receives a form with some fields hidden
+        """
         user = get_user_model()
         user = user.objects.create_user(username="Thiago.Silver", email="Thiago@Silver.com",
-                                 country="DZA", organization="PSG", password="very-secret")
+                                        country="DZA", organization="PSG", password="very-secret")
 
         self.client.login(username=user.username, password="very-secret")
         response = self.client.get(
             u"{}?download_url={}&next={}".format(
                 self.survey_route, urllib.parse.quote("http://example.com"), "/")
         )
-        self.assertEqual(response.context["form"].fields['name'].widget.__class__, forms.HiddenInput().__class__)
-        self.assertEqual(response.context["form"].fields['email'].widget.__class__, forms.HiddenInput().__class__)
-        self.assertEqual(response.context["form"].fields['country'].widget.__class__, forms.HiddenInput().__class__)
-        self.assertEqual(response.context["form"].fields['organization'].widget.__class__, forms.HiddenInput().__class__)
+        self.assertEqual(
+            response.context["form"].fields['name'].widget.__class__, forms.HiddenInput().__class__)
+        self.assertEqual(
+            response.context["form"].fields['email'].widget.__class__, forms.HiddenInput().__class__)
+        self.assertEqual(
+            response.context["form"].fields['country'].widget.__class__, forms.HiddenInput().__class__)
+        self.assertEqual(
+            response.context["form"].fields['organization'].widget.__class__, forms.HiddenInput().__class__)
 
     def test_successfully_survey_submission(self):
+        """
+        Test that successfully submission of the survey form redirects
+        to download and sets survey cookies that prevent survey form view till it expires
+        """
         response = self.client.post(
             u"{}?download_url={}&next={}".format(
                 self.survey_route, urllib.parse.quote("http://example.io"), "/"),
@@ -95,6 +119,9 @@ class TestSurveyView(TestCase):
         self.assertIn("ihp_dlsurvey", response.client.cookies.keys())
 
     def test_failed_survey_submission_with_invalid_email(self):
+        """
+        Test that a user cannot successfully submit a form with an invalid email
+        """
         self.survey_form_data["email"] = "invald-email"
         response = self.client.post(
             u"{}?download_url={}&next={}".format(
@@ -106,6 +133,9 @@ class TestSurveyView(TestCase):
                          "Enter a valid email address."])
 
     def test_failed_survey_submission_with_missing_fields(self):
+        """
+        Test that a user cannot successfully submit a form with missing required fields
+        """
         response = self.client.post(
             u"{}?download_url={}&next={}".format(
                 self.survey_route, urllib.parse.quote("http://example.com"), "/"),
@@ -120,6 +150,10 @@ class TestSurveyView(TestCase):
                          "This field is required."])
 
     def test_direct_download_when_download_survey_is_disabled(self):
+        """
+        Test that all downloads are direct when survey configuration 'survey_enabled' = False
+        i.e. survey is disabled
+        """
         self.survey_config.survey_enabled = False
         self.survey_config.save()
         response = self.client.get(
