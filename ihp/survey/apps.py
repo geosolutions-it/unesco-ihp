d4d0176044f7b6946d9c319b8a3948faad9212c4
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-02 20:32:18 +0300

    Use post_migration signal in app.ready to add default survey configs

--- ihp/survey/apps.py
+++ ihp/survey/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class SurveyConfig(AppConfig):
-    name = 'survey'
+    name = 'ihp.survey'
+    label = 'ihp_survey'
+
+    def ready(self):
+        super(SurveyConfig, self).ready()
+        post_migrate.connect(
+            self.create_default_survey_configuration, sender=self)
+
+    def create_default_survey_configuration(self, *args, **kwargs):
+        SurveyConfiguration = self.get_model('SurveyConfiguration')
+        SurveyConfiguration.load()
