768e52cb8cdd447fed84f13b034be3d30789954d
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-02 20:31:29 +0300

    Modify survey models to be reusable with download approval

--- ihp/survey/models.py
+++ ihp/survey/models.py
@@ -1,15 +1,38 @@
-from django.db import models
-
-from geonode.singleton import SingletonModel
 from datetime import timedelta
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from geonode.base.enumerations import COUNTRIES
+from geonode.singleton import SingletonModel
 
 
 class Survey(models.Model):
-    name = models.CharField(max_length=255)
-    organization = models.CharField(max_length=255)
-    email = models.EmailField()
-    country = models.CharField(max_length=255)
-    reason_for_data_download = models.TextField()
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        related_name=u'survey_user+',
+        null=True,
+        blank=True)
+
+    name = models.CharField(_(u'Name'), max_length=255)
+    organization = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True)
+
+    email = models.EmailField(_(u'Email'))
+
+    country = models.CharField(
+        _(u'Country'),
+        choices=COUNTRIES,
+        max_length=3,
+        blank=True,
+        null=True,)
+
+    reason_for_data_download = models.TextField(_(u'Reason for data download'))
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -17,11 +40,12 @@ class Survey(models.Model):
 
 
 class SurveyConfiguration(SingletonModel):
-    cookie_expiration_time = models.IntegerField('Cookie expiration time in hours', default=24)
+    cookie_expiration_time = models.IntegerField(
+        u'Cookie expiration time in hours', default=24)
     survey_enabled = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name_plural = 'SurveyConfiguration'
+        verbose_name_plural = u'SurveyConfiguration'
 
     def __str__(self):
-        return 'SurveyConfiguration'
+        return u'SurveyConfiguration'
