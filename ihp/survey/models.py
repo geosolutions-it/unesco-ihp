b11555e4f8b65e7a4be41e60a0be364697938557
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-03 19:31:38 +0300

    Add downloaded resource reference to survey model as a ContentType field

--- ihp/survey/models.py
+++ ihp/survey/models.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -33,6 +35,13 @@ class Survey(models.Model):
         null=True,)
 
     reason_for_data_download = models.TextField(_(u'Reason for data download'))
+
+    # reference to the resource that was downloaded
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.SET_NULL, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    downloaded_resource = GenericForeignKey()
+
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
