7ec61029c28399e60e0f5d27eae392b9063464ab
giohappy <giohappy@gmail.com>
2020-04-03 11:40:56 +0200

    disable add/remove survey configuration in django admin

--- ihp/survey/admin.py
+++ ihp/survey/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-
 from ihp.survey.models import Survey, SurveyConfiguration
 
 admin.site.register(Survey)
-admin.site.register(SurveyConfiguration)
+
+@admin.register(SurveyConfiguration)
+class SurveyConfiguration(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
